fb3ef8a6486ef33bb401bbd06ac584076f7f3868
FrakerKill <ronsanbern@hotmail.com>
2024-10-23 16:27:27 +0200

    fixing telegram errors

--- binance/services/TelegramNotify.js
+++ binance/services/TelegramNotify.js
@@ -1,31 +1,32 @@
-const axios = require('axios').default;
-const urlencode = require('urlencode');
-const moment = require('moment')
+const axios = require('axios').default
+const urlencode = require('urlencode')
+const diff = Date.now()
+const timenow = moment.utc(diff).format('HH:mm:ss')
 
 const NotifyTelegram = async (data) => {
-    const b = "`";
+    const b = "`"
     const content = urlencode(`
 
-${b + b + b}
-${data.from === 'buy' ? '🟢' : data.from === 'sell' ? '🔴' : '🔵'} ${moment.utc(Date.now()).format('DD/MM/YYYY HH:mm:ss')}
-${b + b + b}
+    ${b + b + b}
+    ${data.from === 'buy' ? '🟢' : data.from === 'sell' ? '🔴' : '🔵'} ${timenow}
+    ${b + b + b}
 __Inicio Bot:__ ${data.start}\\
 __Duración:__ ${data.runningTime}\\
 __Mercado:__ ${data.market}\\
 __Precio ${data.market1}:__ ${data.price}\\
 __Saldo ${data.market1}:__ ${data.balance1}\\
 __Saldo ${data.market2}:__ ${parseFloat(data.balance2).toFixed(2)}\\
-__Profits:__ ${parseFloat(data.realProfits).toFixed(2)} ${data.market2} ${data.market2 < 0 ? '🔻' : '↗' }\\
+__Profits:__ ${parseFloat(data.realProfits).toFixed(2)} ${data.market2} ${data.market2 < 0 ? '🔻' : '🔺' }\\
 __Orden Compra:__ ${parseFloat(data.openorders)}\\
-`).replace(/\./g, '\\.')
+    `).replace(/\./g, '\\.')
 
     try {
-        await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_ID}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&parse_mode=MarkdownV2&text=${content}`);
+        await axios.get(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_ID}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&parse_mode=MarkdownV2&text=${content}`)
     } catch (err) {
-        console.error('Error enviando mensaje a Telegram:', err);
+        console.error('Error enviando mensaje a Telegram:', err)
     }
-};
+}
 
 module.exports = {
     NotifyTelegram
-};
+}
